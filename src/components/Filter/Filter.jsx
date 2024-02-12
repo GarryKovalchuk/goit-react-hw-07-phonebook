98e4e3d22b0dd63770d29c510f0b41514e3ce98c
Garry <vorzel47051@gmail.com>
2024-02-12 20:43:24 +0200

    react07

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,24 @@
+import './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, getFilter } from '../../redux/slice';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  const onChangeFilter = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <label>
+      <input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={onChangeFilter}
+        placeholder="Find contacts by name"
+      />
+    </label>
+  );
+};
