5266a37547fd66bef16b13fce489548b68e83541
Garry <vorzel47051@gmail.com>
2024-02-12 20:23:34 +0200

    react07

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './ContactForm.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, addContact } from '../../redux/slice';
+
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const contact = { name, number };
+    const isExists = contacts.some(
+      i => i.name.toLowerCase() === contact.name.toLowerCase()
+    );
+    if (isExists) {
+      alert(`${name} or ${number} is already in contacts`);
+    } else {
+      dispatch(addContact(contact));
+    }
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        placeholder="Name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Назва може містити лише літери, апостроф, тире та пробіли. Наприклад Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+
+      <input
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        placeholder="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Номер телефону має складатися з цифр і може містити пробіли, тире, круглі дужки та може починатися з +"
+        required
+      />
+
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
