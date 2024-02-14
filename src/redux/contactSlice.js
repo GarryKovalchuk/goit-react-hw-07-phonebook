44fcf3699f4e7000d1e24661b06205efdc126210
Garry <vorzel47051@gmail.com>
2024-02-14 22:33:20 +0200

    react07

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const contactsApi = createApi({
+  reducerPath: 'contacts',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://65cb464aefec34d9ed872342.mockapi.io/api/react07',
+  }),
+  tagTypes: ['contacts'],
+  endpoints: builder => ({
+    getContacts: builder.query({
+      query: () => `/contacts`,
+      providesTags: ['contacts'],
+    }),
+
+    deleteContact: builder.mutation({
+      query: id => ({
+        url: `/contacts/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['contacts'],
+    }),
+
+    addContact: builder.mutation({
+      query: values => ({
+        url: `/contacts`,
+        method: 'POST',
+        body: values,
+      }),
+      invalidatesTags: ['contacts'],
+    }),
+  }),
+});
+
+export const {
+  useGetContactsQuery,
+  useDeleteContactMutation,
+  useAddContactMutation,
+} = contactsApi;
